586390eebc784ce2d7df031507bb9a738fdf5ad2
Chris W Jones <Christopher.Jones@sourceability.com>
2023-06-13 14:00:02 -0400

    Fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,19 @@
 
 declare(strict_types = 1);
 
+use Consistence\Doctrine\Enum\EnumAnnotation;
 use Consistence\Doctrine\Enum\Type\BooleanEnumType;
 use Consistence\Doctrine\Enum\Type\FloatEnumType;
 use Consistence\Doctrine\Enum\Type\IntegerEnumType;
 use Consistence\Doctrine\Enum\Type\StringEnumType;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\DBAL\Types\Type as DoctrineType;
 
 error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+AnnotationRegistry::loadAnnotationClass(EnumAnnotation::class);
 
 DoctrineType::addType(FloatEnumType::NAME, FloatEnumType::class);
 DoctrineType::addType(IntegerEnumType::NAME, IntegerEnumType::class);
